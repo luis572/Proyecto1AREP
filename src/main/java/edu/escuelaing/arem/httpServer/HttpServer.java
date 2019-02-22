b91c78fa9fe3527c9755a3da3afcf2e9126ee8b9
luis572 <luispg572@gmail.com>
2019-02-22 18:04:40 -0500

    correccion

--- src/main/java/edu/escuelaing/arem/httpServer/HttpServer.java
+++ src/main/java/edu/escuelaing/arem/httpServer/HttpServer.java
@@ -69,8 +69,6 @@ public class HttpServer {
                         
                         in.close();
                         clientSocket.close();
-                        serverSocket.close();
-                        serverSocket=SocketServidor.servidor();
 		}
 	}
     	public static String getPageRequest(InputStream is) throws IOException {
